d652e0d1180cb9b6e7e107f284f914a219c11e51
Dymock Brett <22858501+dymockb@users.noreply.github.com>
2024-03-25 16:22:51 +0000

    Update constants.tsx

--- frontend/app/utils/constants.tsx
+++ frontend/app/utils/constants.tsx
@@ -1,2 +1,2 @@
-const apiBasePath = "https://chat-langchainjs-frontend-7j5u-9eezsav49-dymock-bs-projects.vercel.app"
+const apiBasePath = "https://chat-langchainjs-frontend-7j5u-9eezsav49-dymock-bs-projects.vercel.app";
 export const apiBaseUrl = `${apiBasePath}/api`;
