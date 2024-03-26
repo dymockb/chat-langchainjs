b2d42d6c240d3d48bf0b8ed18cb8982c8de6039e
Dymock Brett <22858501+dymockb@users.noreply.github.com>
2024-03-26 09:14:56 +0000

    Update ChatWindow.tsx

--- frontend/app/components/ChatWindow.tsx
+++ frontend/app/components/ChatWindow.tsx
@@ -98,8 +98,6 @@ export function ChatWindow(props: { conversationId: string }) {
     marked.setOptions({ renderer });
 
     try {
-      console.log("test")
-      console.log(process.env.OPENAI_API_KEY)
       const sourceStepName = "FindDocs";
       let streamedResponse: Record<string, any> = {};
       const remoteChain = new RemoteRunnable({
